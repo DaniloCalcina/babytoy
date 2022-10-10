4420604bfdc9b5cbad3692e43c075a5b15943fe2
Danilo Calcina <danilocalcina27@gmail.com>
2022-10-10 00:34:55 -0300

    Final

--- src/components/Formulary/Formulary.jsx
+++ src/components/Formulary/Formulary.jsx
@@ -4,13 +4,24 @@ import Form from 'react-bootstrap/Form';
 import './Formulary.css';
 import { useNavigate  } from "react-router-dom";
 import { useUserContext } from '../../context/UserContext';
+import swal from 'sweetalert';
 
 
 
 export default function Formulary() {
-    const navigate = useNavigate();
- /*    const {totalPrice,cart:carrito,clearCart}= useCartContext(); */
+const navigate = useNavigate();
 const {addUser, verifyUser} = useUserContext();
+
+const Bienvenida = () => {
+
+    swal ({
+        title: "Inicio Exitoso",
+        text: "Bienvenido",
+        icon: "success",
+        button: "Iniciar"
+    }).then (()=>{navigate('/');})
+}
+
 const sendUser = (e) =>{
     e.preventDefault();
     const buyer={
@@ -21,8 +32,9 @@ const sendUser = (e) =>{
         Adress:e.target[4].value,
         }
     addUser(buyer);
-    navigate('/');
+    Bienvenida();
 }
+
 const verifUser= (e)=> {
     e.preventDefault();
     const member ={
@@ -30,12 +42,15 @@ const verifUser= (e)=> {
         Password:e.target[1].value,
     }
     verifyUser (member);
-    navigate('/');
+    Bienvenida();
 }
 
   return (
-    <div className="d-flex justify-content-center align-items-center flex-column">
-        <img className='mt-2' src="assets/leon.png" alt="imagen" height={240} width={300}/>
+    <div className="d-flex justify-content-center align-items-center flex-column page-container mt-1 p-1">
+      <div className="logo_container d-flex m-5">
+        <div className="text_logo me-5">BabyStore</div>
+        <img src="assets/leon.png" alt="imagen" height={160} width={200}/>
+      </div>
     <Container id='principal-container' className='d-flex justify-content-center align-items-center'>
         <div className="d-flex">
         <div className="m-1 me-5">
